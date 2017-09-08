c3e7049814314ef09a3855cba495a528669f0aa4
12AbelCabreraMiranda <leg.abelr@gmail.com>
2017-09-08 04:04:22 -0600

    Script de Like SQLServer
    Este es un pequeño ejemplo de la utilizacion de la funcion Like en SQLServer2014

---
+++ like.sql
@@ -0,0 +1,5 @@
+select *from clientes
+
+select *from clientes where nombreCliente= 'juan' --- Muestra todo los nombres con juan
+select *from clientes where nombreCliente like 'e%'-- Muestra todo los nombres que empiezan con E
+select *from clientes where nombreCliente like '%i%'-- Muestra todo los nombres que contengan la letra 'i'
